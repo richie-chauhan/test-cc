da94d328dc3c876a3bf5c4310c886830a346533a
Richie Chauhan <richie.chauhan@gmail.com>
2020-01-12 18:49:05 +0530

    Bad Code

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static final int myage = 15;
+    public final int yourage = 20;
+
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+
+        if(yourage > 15)
+            System.out.println("I am younger than you");
+    } 
+} 
+
